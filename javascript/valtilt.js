626659f9eb255a26ba7df1f061fff7a4f56406d9
Jessie <126818085+jessieservis@users.noreply.github.com>
2025-09-17 11:54:40 -0400

    Transitioned to a Different Project

--- javascript/valtilt.js
+++ javascript/valtilt.js
@@ -1,8 +1,8 @@
-let tilt = document.querySelectorAll(".rounded");
+let tilt = document.querySelectorAll('.rounded');
 VanillaTilt.init(tilt, {
   max: 3,
   speed: 500,
   scale: 1.05,
   glare: true,
-  "max-glare": 0.3,
+  'max-glare': 0.3,
 });
